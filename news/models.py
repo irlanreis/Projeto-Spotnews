71ffee923e0bc41f07bf4205b111b6a17b92370c
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-21 09:59:40 -0400

    Feat: Corrige Bug na model

--- news/models.py
+++ news/models.py
@@ -23,10 +23,10 @@ class User(models.Model):
 class News(models.Model):
     title = models.CharField(max_length=200, validators=[validate_title])
     content = models.TextField()
-    author = models.ForeignKey("User", on_delete=models.CASCADE)
-    categories = models.ManyToManyField("Category")
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    categories = models.ManyToManyField('Category')
     created_at = models.DateField()
-    image = models.ImageField(upload_to='img/', null=True, blank=True)
+    image = models.ImageField(upload_to="img/", null=True, blank=True)
 
     def __str__(self):
         return self.title
--- news/validators.py
+++ news/validators.py
@@ -4,5 +4,5 @@ from django.core.exceptions import ValidationError
 def validate_title(value):
     letters = value.split()
 
-    if len(letters) < 1:
+    if len(letters) == 1:
         raise ValidationError('O titulo deve conter pelo menos 2 palavras')
