2fef11437b580abe115e91440f51a3f736375a19
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-21 10:30:40 -0400

    Feat: Correção de sintaxe.

--- news/models.py
+++ news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import ValidationError
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -23,14 +23,14 @@ def validate_title(value):
     letters = value.split()
 
     if len(letters) == 1:
-        raise ValidationError('O titulo deve conter pelo menos 2 palavras.')
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
 
 
 class News(models.Model):
     title = models.CharField(max_length=200, validators=[validate_title])
     content = models.TextField()
-    author = models.ForeignKey('User', on_delete=models.CASCADE)
-    categories = models.ManyToManyField('Category')
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    categories = models.ManyToManyField("Category")
     created_at = models.DateField()
     image = models.ImageField(upload_to="img/", null=True, blank=True)
 
