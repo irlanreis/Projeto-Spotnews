97214e21f472607a656526e4c9de89fdc57d46c7
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-21 22:17:13 -0400

    Feat: Finaliza Projeto :rocket:

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -22,11 +22,13 @@ from news import views
 from rest_framework.routers import DefaultRouter
 from news_rest.views.category_view import CategoryViewSet
 from news_rest.views.user_view import UserViewSet
+from news_rest.views.news_view import NewsViewSet
 
 
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet)
 router.register(r"users", UserViewSet)
+router.register(r"news", NewsViewSet)
 
 
 urlpatterns = [
